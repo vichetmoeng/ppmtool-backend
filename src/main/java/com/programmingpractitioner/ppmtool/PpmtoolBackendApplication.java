044a16d3f0be15d99d2699462b6676dd33f13101
Vichet MOENG <54062393+vichetmoeng@users.noreply.github.com>
2023-02-03 08:18:12 +0700

    init project

---
+++ src/main/java/com/programmingpractitioner/ppmtool/PpmtoolBackendApplication.java
@@ -0,0 +1,13 @@
+package com.programmingpractitioner.ppmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmtoolBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PpmtoolBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/programmingpractitioner/ppmtool/PpmtoolBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programmingpractitioner.ppmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmtoolBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
